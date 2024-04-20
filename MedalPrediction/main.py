08251a50747846c86cecb904a37988d1e3319e24
POP <pop@pop-os.localdomain>
2024-04-20 18:29:29 +0545

    main created

---
+++ MedalPrediction/main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+
+class PlayerMain:
+
+    df = pd.read_csv('/home/pop/Desktop/python/MachineLearning/LinearRegression/MedalPrediction/atheletes.csv')
+    
